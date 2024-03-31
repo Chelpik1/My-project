682f6ef86cca20cbd43408dfcf7404f9317a59c9
Chelpik1 <artemchebotarew@ya.ru>
2024-03-31 23:11:46 +0300

    dz4

---
+++ Assets/Скрипты/Grenade1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade1 : MonoBehaviour
+{
+    public float delay = 3;
+    public GameObject explosionPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+        Invoke("Explosion", 3);
+    }
+    private void Explosion()
+    {
+        Destroy(gameObject);
+        var explosion = Instantiate(explosionPrefab);
+        explosion.transform.position = transform.position;
+    }
+}
